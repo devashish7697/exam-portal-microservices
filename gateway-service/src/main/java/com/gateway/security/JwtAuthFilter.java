18f1ed068aaec33ce0b56e6fc0764910d4083b3a
Devashish Sharma <sharmadevashish125@gmail.com>
2025-10-16 19:58:24 +0530

    Updated Docker-compose with Gateway-service

--- gateway-service/src/main/java/com/gateway/security/JwtAuthFilter.java
+++ gateway-service/src/main/java/com/gateway/security/JwtAuthFilter.java
@@ -1,4 +1,4 @@
-package security;
+package com.gateway.security;
 
 import io.jsonwebtoken.Claims;
 import org.springframework.http.HttpHeaders;
--- gateway-service/src/main/java/com/gateway/security/JwtUtil.java
+++ gateway-service/src/main/java/com/gateway/security/JwtUtil.java
@@ -1,4 +1,4 @@
-package security;
+package com.gateway.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
